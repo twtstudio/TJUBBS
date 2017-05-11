724702fe6123b1762aff8b7fbd8e825a5cf70bf7
Halcao <nyz1500@gmail.com>
2017-05-11 23:04:45 +0800

    modified the ui and add a new type to InfoModifyController

--- TJUBBS/LoginViewController.swift
+++ TJUBBS/LoginViewController.swift
@@ -178,6 +178,8 @@ class LoginViewController: UIViewController {
 
                     }
                 }
+            } else {
+                
             }
         }
         
@@ -224,7 +226,7 @@ class LoginViewController: UIViewController {
             make.right.equalTo(loginButton!.snp.right)
         }
         authenticateButton?.addTarget { _ in
-            let vc = InfoModifyController(title: "老用户认证", items: ["用户名-输入用户名-username", "姓名-输入姓名-name", "身份证号-输入身份证号-id"], style: .bottom, headerMsg: "老用户（即已拥有BBS账号）请填写以下信息认证") { result in
+            let vc = InfoModifyController(title: "老用户认证", items: ["用户名-输入用户名-username", "姓名-输入姓名-name", "身份证号-输入身份证号-id", "备注-补充说明其他信息证明您的身份，如曾经发过的帖子名、注册时间、注册邮箱、注册时所填住址等-comment-v"], style: .bottom, headerMsg: "老用户（即已拥有BBS账号）请填写以下信息认证") { result in
                 print(result)
                 // TODO: 逻辑判断
                 let vc =  InfoModifyController(title: "老用户认证", items: ["新密码-输入新密码-newpass-s", "再次确认-输入新密码-ensure-s"], style: .bottom, headerMsg: "请重置密码，以同步您的个人数据") { result in
--- TJUBBS/TextInputCell.swift
+++ TJUBBS/TextInputCell.swift
@@ -8,35 +8,62 @@
 
 import UIKit
 
+enum TextInputCellType {
+    case textField
+    case textView
+}
+
 class TextInputCell: UITableViewCell {
     
     var titleLabel: UILabel?
     var textField: UITextField?
+    var textView: UITextView?
+    var type: TextInputCellType = .textField
 
-    convenience init(title: String, placeholder: String) {
+    convenience init(title: String, placeholder: String, type: TextInputCellType = .textField) {
         self.init()
-        
-        textField = UITextField()
-        contentView.addSubview(textField!)
-        textField?.snp.makeConstraints {
-            make in
-            make.centerY.equalTo(contentView)
-            make.right.equalTo(contentView.snp.right).offset(-16)
+        if type == .textField {
+            textField = UITextField()
+            contentView.addSubview(textField!)
+            textField?.snp.makeConstraints {
+                make in
+                make.centerY.equalTo(contentView)
+                make.right.equalTo(contentView).offset(-16)
+            }
+            textField?.placeholder = placeholder
+            textField?.textAlignment = .right
+            
+            titleLabel = UILabel(text: title)
+            contentView.addSubview(titleLabel!)
+            titleLabel?.snp.makeConstraints {
+                make in
+                make.left.equalTo(contentView).offset(16)
+                make.centerY.equalTo(contentView)
+                make.right.equalTo(textField!.snp.left).offset(-8)
+            }
+        } else if type == .textView {
+            titleLabel = UILabel(text: title)
+            titleLabel?.sizeToFit()
+            contentView.addSubview(titleLabel!)
+            titleLabel?.snp.makeConstraints {
+                make in
+                make.left.equalTo(contentView).offset(16)
+                make.top.equalTo(contentView).offset(10)
+            }
+
+            textView = UITextView()
+            contentView.addSubview(textView!)
+            textView?.snp.makeConstraints {
+                make in
+                make.top.equalTo(titleLabel!.snp.bottom).offset(10)
+                make.height.equalTo(60)
+                make.right.equalTo(contentView).offset(-16)
+                make.left.equalTo(contentView).offset(16)
+                make.bottom.equalTo(contentView).offset(-20)
+            }
         }
-        textField?.placeholder = placeholder
-        textField?.textAlignment = .right
-        
-        titleLabel = UILabel(text: title)
-        contentView.addSubview(titleLabel!)
-        titleLabel?.snp.makeConstraints {
-            make in
-            make.left.equalTo(contentView).offset(16)
-            make.centerY.equalTo(contentView)
-            make.right.equalTo(textField!.snp.left).offset(-8)
-        }
-    
-        
     }
+
     
     override func awakeFromNib() {
         super.awakeFromNib()
