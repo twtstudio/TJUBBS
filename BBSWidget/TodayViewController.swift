63ac6ee3e361b5f64814fabd0d0e63531dacb9dc
Halcao <nyz1500@gmail.com>
2017-08-20 23:27:31 +0800

    add today widget

---
+++ BBSWidget/TodayViewController.swift
@@ -0,0 +1,75 @@
+//
+//  TodayViewController.swift
+//  BBSWidget
+//
+//  Created by Halcao on 2017/8/18.
+//  Copyright © 2017年 twtstudio. All rights reserved.
+//
+
+import UIKit
+import NotificationCenter
+import ObjectMapper
+import Alamofire
+
+class TodayViewController: UIViewController, NCWidgetProviding {
+        
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var replyLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(viewOnTap(sender:)))
+        self.view.addGestureRecognizer(tapGesture)
+        // Do any additional setup after loading the view from its nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func viewOnTap(sender: UITapGestureRecognizer) {
+        self.extensionContext?.open(URL(string: "openTJUBBS://?tid=\(self.titleLabel.tag)")!, completionHandler: nil)
+//        UIApplication.shared.openURL(URL(string: "openTJUBBS://?tid=\(self.titleLabel.tag)")!)
+    }
+    
+    func widgetPerformUpdate(completionHandler: (@escaping (NCUpdateResult) -> Void)) {
+        // Perform any setup necessary in order to update the view.
+        Alamofire.request("https://bbs.twtstudio.com/api/index", method: .get, parameters: nil, encoding: JSONEncoding.default, headers: nil).responseString { response in
+            switch response.result {
+            case .success:
+                if let data = response.data {
+                    do {
+                        let json = try JSONSerialization.jsonObject(with: data, options: .allowFragments)
+                        if let dict = json as? Dictionary<String, AnyObject> {
+                            if let err = dict["err"] as? Int, err == 0, let data = dict["data"] as? Dictionary<String, Any>,
+                                let hot = data["hot"] as? Array<Dictionary<String, Any>> {
+                                let threadList = Mapper<ThreadModel>().mapArray(JSONArray: hot)
+                                let random = abs(arc4random().distance(to: 0)) % threadList.count
+                                let thread = threadList[random]
+                                self.titleLabel.text = thread.title
+                                self.replyLabel.text = "回复 (\(thread.replyNumber))"
+                                // keep the thread id
+                                self.titleLabel.tag = thread.id
+                                completionHandler(NCUpdateResult.newData)
+                            } else {
+                                completionHandler(NCUpdateResult.failed)
+                            }
+                        }
+                    } catch _ {
+                        completionHandler(NCUpdateResult.failed)
+                        // log.error(error)/
+                    }
+                }
+            case .failure( _):
+                completionHandler(NCUpdateResult.failed)
+                // log.error(error)/
+            }
+        }
+        
+        // If an error is encountered, use NCUpdateResult.Failed
+        // If there's no update required, use NCUpdateResult.NoData
+        // If there's an update, use NCUpdateResult.NewData
+        
+    }
+    
+}
--- Podfile
+++ Podfile
@@ -1,18 +1,27 @@
 # Uncomment the next line to define a global platform for your project
 platform :ios, '9.0'
   use_frameworks!
-target 'TJUBBS' do
+
+def shared_pods
+  pod 'PiwikTracker', git: 'https://github.com/piwik/piwik-sdk-ios.git', branch: 'master'
+  pod 'ObjectMapper'
   pod 'Alamofire'
+end
+
+target 'TJUBBS' do
+  shared_pods
   pod 'SnapKit'
   pod 'PKHUD', '~> 4.0'
   pod 'MJRefresh', '~> 3.1.12'
   pod 'WMPageController', '2.3.0'
-  pod 'ObjectMapper'
   pod 'SlackTextViewController'
   pod 'Kingfisher'
   pod 'DTCoreText'
   pod 'Marklight'
   pod 'YYText'
   #pod 'PiwikTracker', '~> 4.0.0-beta'
-  pod 'PiwikTracker', git: 'https://github.com/piwik/piwik-sdk-ios.git', branch: 'master'
+end
+
+target 'BBSWidget' do
+	shared_pods
 end
