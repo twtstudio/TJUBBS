90eb14013586a742a27b70180b1adb2b8e8469ca
SilentStars <h15822008350@163.com>
2018-11-01 10:03:46 +0800

    Hottest thread view, activity thread view and announce view. Fix bugs in thread detail view and some others.

---
+++ TJUBBS/HotThreadTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  HotThreadTableViewCell.swift
+//  TJUBBS
+//
+//  Created by 侯钦瀚 on 2018/10/16.
+//  Copyright © 2018年 twtstudio. All rights reserved.
+//
+
+import UIKit
+
+class HotThreadTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
